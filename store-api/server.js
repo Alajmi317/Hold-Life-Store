561fd93459e5a907abc9f93e00b35bd5969024d4
Alajmi317 <alajmi.9732@gmail.com>
2026-02-19 12:55:37 +0300

    Update server.js

--- store-api/server.js
+++ store-api/server.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const DB_FILE = "products.json";
+
+// جلب المنتجات
+app.get("/api/products", (req, res) => {
+  const data = fs.readFileSync(DB_FILE);
+  res.json(JSON.parse(data));
+});
+
+// إضافة منتج من لوحة الأدمن
+app.post("/api/products", (req, res) => {
+  const products = JSON.parse(fs.readFileSync(DB_FILE));
+  products.push(req.body);
+  fs.writeFileSync(DB_FILE, JSON.stringify(products, null, 2));
+  res.json({ success: true });
+});
+
+app.listen(3000, () => {
+  console.log("API شغال على http://localhost:3000");
+});
 
